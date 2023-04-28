0bfa3561472c0f5bfa21761ffce3090c2e1f4193
Saulo Vargas <sauloco022@gmail.com>
2023-04-28 18:03:16 -0300

    added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,6 +104,8 @@ const Home: React.FC = () => {
 				<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
 				<meta name="description" content="Task manager from Rustic IT"/>
 				<meta name="author" content="Saulo Vargas | saulo@rusticit.com"/>
+				<link rel="icon"
+				      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✅</text></svg>"/>
 			</Head>
 			<h1
 				className={`
