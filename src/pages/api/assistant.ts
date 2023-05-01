1e26975e9bc1340764a7daa975dca0b6f424f2e7
Saulo Vargas <sauloco022@gmail.com>
2023-05-01 00:12:38 -0300

    improved prompt added todo and ideas in README.md

--- src/pages/api/assistant.ts
+++ src/pages/api/assistant.ts
@@ -12,20 +12,29 @@ const api = new ChatGPTAPI({
 
 const systemMessage = `
 Act as a personal assistant.
-I will tell you a task I need to do, you will only return a minimal and concise list of no more of 3 subtasks to accomplish the given task with the suggested time reference for when the subtask must be performed, if the task is simple enough simply return it as is.
+I will tell you a task I need to do, you will only return a minimal and concise list of one, two, three or four subtasks to accomplish the given task with the suggested time reference for when the subtask must be performed, if the task is simple enough simply return it as is.
 
+---
 Example 1
 buy milk tomorrow. Current date: 2023/04/29
 Returns: 
-[2023/04/30] Buy milk. Description: Sourdough or multigram.
-
-
+2023/04/30 | Buy milk.
+---
 Example 2
 Prompt: cook diner tomorrow. Current date: 2023/04/29 
 Returns: 
-- [2023/04/29 9:00] Check fridge and pantry for all the ingredients. 
-- [2023/04/29 16:00] Go to grocery store or market for any ingredient.
-- [2023/04/29 18:00] Cook dinner.
+2023/04/29 9:00 | Check fridge and pantry for all the ingredients. 
+2023/04/29 16:00 | Go to grocery store or market for any ingredient.
+2023/04/29 18:00 | Cook dinner.
+---
+Example 3
+Prompt: prepare for Maths exam on next friday. Description: alan has the notes. Current date: 2023/05/01
+Returns:
+2023/05/02 | Ask Alan for Mathematics notes.
+2023/05/03 | Study Mathematics for 2 hours.
+2023/05/04 | Study Mathematics for 2 hours.
+2023/05/05 | Present for Mathematics exam.
+---
 `
 
 interface AssistantQuery {
@@ -55,8 +64,8 @@ export default async function handler(
 		let chatRes = await api.sendMessage(enhancedPrompt, {systemMessage})
 		const items: Item[] = []
 		for (const line of chatRes.text.split('\n')) {
-			if (line.includes('] ')) {
-				const [date, task] = line.split('- [').join('').split('] ');
+			if (line.includes(' | ')) {
+				const [date, task] = line.split(' | ');
 				const dateCompletion = new Date(date).getTime();
 				items.push({
 					id: v4(),
